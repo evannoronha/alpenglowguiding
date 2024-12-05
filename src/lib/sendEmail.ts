1c59e81d4f2e2de9ed1b8d375a2167b3d366a28e
Evan Noronha <hello@evannoronha.com>
2024-12-04 22:44:13 -0800

    Access env variables using import.meta.env
    Apparently this will allow it to work with cloudflare workers

--- src/lib/sendEmail.ts
+++ src/lib/sendEmail.ts
@@ -1,11 +1,11 @@
 import { Resend } from "resend";
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(import.meta.env.RESEND_API_KEY);
 
 export const sendEmail = async (name: string, email: string, htmlContent: string) => {
     const sendResend = await resend.emails.send({
-        from: process.env.RESEND_FROM_EMAIL as string,
+        from: import.meta.env.RESEND_FROM_EMAIL as string,
         replyTo: email as string,
-        to: process.env.RESEND_TO_EMAIL as string,
+        to: import.meta.env.RESEND_TO_EMAIL as string,
         subject: `Submission from ${name}`,
         html: htmlContent,
     }); // If the message was sent successfully, return a 200 response
