d37a05c0ee63c9ceae76b3385ded4cf527c920b1
Evan Noronha <hello@evannoronha.com>
2024-12-04 22:27:42 -0800

    Remove comment

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -3,8 +3,6 @@ export const prerender = false; //This will not work without this line
 import type { APIRoute } from "astro";
 import { sendEmail } from "../../lib/sendEmail";
 
-// Instantiate Resend with api key in .env called RESEND_API_KEY
-
 export const POST: APIRoute = async ({ request }) => {
   const data = await request.formData();
   const name = data.get("name") as string;
