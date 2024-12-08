b34f188e37a53172727dd7c1d1e865f9fe2958c7
Evan Noronha <hello@evannoronha.com>
2024-12-08 13:26:48 -0800

    Make the lighthouse audit reflect our priorities

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -5,6 +5,10 @@ module.exports = {
       },
       assert: {
         preset: 'lighthouse:recommended',
+        assertions: {
+          "categories:performance": ["warn", {"minScore": 0.95}],
+          "categories:seo": ["error", {"minScore": 1}],
+        }
       },
     },
   staticDistDir: "./dist",
