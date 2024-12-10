95961d8621f2bc1721abcfac70c01d900c5e35da
Evan Noronha <hello@evannoronha.com>
2024-12-10 12:49:22 -0800

    Lighthouse: Ding us for not using responsive images
    my body is ready.

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -12,7 +12,6 @@ module.exports = {
         assertions: {
           "categories:performance": ["error", {"minScore": 0.95}],
           "categories:seo": ["error", {"minScore": 1}],
-          "uses-responsive-images": "off",
           "unused-javascript": "off",
         }
       },
