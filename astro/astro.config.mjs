294e02aa53ceff43f70bfd127f76e0776c1b585f
Evan Noronha <hello@evannoronha.com>
2025-10-31 12:43:15 -0600

    Remove robots and sitemap plugins from astro config
    We'll be rolling our own now.

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -5,10 +5,6 @@ import cloudflare from '@astrojs/cloudflare';
 
 import react from '@astrojs/react';
 
-import robotsTxt from 'astro-robots-txt';
-
-import sitemap from '@astrojs/sitemap';
-
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
@@ -17,14 +13,7 @@ export default defineConfig({
   }),
   output: "server",
   site: 'https://alpenglowguiding.com',
-  integrations: [react(), robotsTxt({
-    sitemap: false
-  }), sitemap({
-    serialize: (item) => {
-      item.url = item.url.replace(/\/$/, "");
-      return item;
-    }
-  }), mdx()],
+  integrations: [react(), mdx()],
   trailingSlash: 'never',
   image: {
     service: passthroughImageService(),
