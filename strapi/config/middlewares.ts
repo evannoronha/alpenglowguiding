c55df9612c5a4675c1d3a67d62fe6ed105aceab4
Evan Noronha <hello@evannoronha.com>
2025-10-31 17:18:34 -0600

    increase max size

--- strapi/config/middlewares.ts
+++ strapi/config/middlewares.ts
@@ -16,7 +16,17 @@ export default ({ env }) => [
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
-  'strapi::body',
+  {
+    name: 'strapi::body',
+    config: {
+      formLimit: '1gb',
+      jsonLimit: '1gb',
+      textLimit: '1gb',
+      formidable: {
+        maxFileSize: 1024 * 1024 * 1024, // 1gb in bytes
+      },
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
