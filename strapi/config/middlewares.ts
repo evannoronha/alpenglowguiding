0e9a25ced28aa976fdda2723da157e717b335421
Evan Noronha <hello@evannoronha.com>
2025-10-31 02:05:10 -0600

    Implement preview mode for strapi editor

--- strapi/config/middlewares.ts
+++ strapi/config/middlewares.ts
@@ -1,7 +1,18 @@
-export default [
+export default ({ env }) => [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'frame-src': ["'self'", env('CLIENT_URL', 'http://localhost:4321')],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
